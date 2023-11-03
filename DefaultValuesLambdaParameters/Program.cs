0bb805bed7722d2baa95e4801cae8b71aa506092
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-11-03 08:28:57 +0100

    Default lambda deal usage

--- DefaultValuesLambdaParameters/Program.cs
+++ DefaultValuesLambdaParameters/Program.cs
@@ -6,3 +6,7 @@ var isSameLength = (int lengthA, int lengthB = -1) => lengthA == lengthB;
 Console.WriteLine(isSameLength(10, 10));
 Console.WriteLine(isSameLength(10, 2));
 Console.WriteLine(isSameLength(10));
+
+
+// Real usefull usage Minimal API
+// app.MapGet("getblogposts", async (MyService service, int? page = 1, int? pageSize = 50) => { }
