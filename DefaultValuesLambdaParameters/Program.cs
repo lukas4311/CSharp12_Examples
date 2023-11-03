1c4322be2d46f4ab643225b83d3c790c722f1f52
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-11-03 08:24:30 +0100

    New features

---
+++ DefaultValuesLambdaParameters/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var isSameLength = (int lengthA, int lengthB = -1) => lengthA == lengthB;
+
+Console.WriteLine(isSameLength(10, 10));
+Console.WriteLine(isSameLength(10, 2));
+Console.WriteLine(isSameLength(10));
---
+++ TypeAlias/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GeoLocation = (int lat, int lng);
+
+
+
+Console.WriteLine("Hello, World!");
+
+class DistanceCalculator(GeoLocation currentLocation)
+{
+    public int CalculateDistance(GeoLocation desination)
+    {
+        int distance = calculateDistanceAlgo(currentLocation, desination);
+        return distance;
+    }
+}
