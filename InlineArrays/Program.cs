c5bfb8ee7ca880969d8bc32f7e010e57e696169c
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-11-07 15:12:12 +0100

    New features

---
+++ InlineArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿// Array in struct type
+// Fixed size
+// Cannot use LINQ for them (not implement IEnumerable)
+
+var arr = new MyInlineArray();
+
+for (int i = 0; i < 10; i++)
+{
+    arr[i] = new String(i.ToString() + "aa");
+}
+
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+
+[System.Runtime.CompilerServices.InlineArray(10)]
+public struct MyInlineArray
+{
+    private object _element;
+}
