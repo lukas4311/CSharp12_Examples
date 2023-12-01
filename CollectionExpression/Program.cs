7ff7e3f3c09c4323c064ace0fe448d5763def8bc
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-12-01 08:24:09 +0100

    New example collection expression

--- CollectionExpression/Program.cs
+++ CollectionExpression/Program.cs
@@ -3,7 +3,11 @@ Console.WriteLine("Hello, World!");
 
 int[] array = [1, 2, 3, 4, 5, 6, 7];
 
+// Spread operator
 int[] array2 = [8, 9, .. array];
+
+bool condition = true;
+int[] array3 = [.. condition ? array : [], 8, 9];
 
 for (int i = 0; i < array2.Length; i++)
     Console.WriteLine(array2[i]);
