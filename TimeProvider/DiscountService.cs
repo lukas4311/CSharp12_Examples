81a221186b65db634002fa2b2897d39a44d4f17a
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-12-01 08:26:43 +0100

    Time provider

--- TimeProvider/DiscountService.cs
+++ TimeProvider/DiscountService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TimeProviderNspc
+﻿namespace TimeProviderNspc
 {
     public class DiscountService : IDiscountService
     {
--- TimeProvider/Program.cs
+++ TimeProvider/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TimeProviderNspc;
 
+// solving problem with unit testing logic using DateTime.Now 
+// unit test are problem cause you dont have control over current time
+
 Console.WriteLine("Hello, World!");
 
 var discountService = new DiscountService(TimeProvider.System);
+
 discountService.CalculateDiscount();
+
+// TimeProvider is abstract class and can be mocked in unit tests
