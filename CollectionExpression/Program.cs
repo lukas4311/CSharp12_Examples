405b738cc1c7449d591fd76f1c9c624dd68e28be
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-12-01 07:11:20 +0100

    Collection expression

---
+++ CollectionExpression/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] array = [1, 2, 3, 4, 5, 6, 7];
+
+int[] array2 = [8, 9, .. array];
+
+for (int i = 0; i < array2.Length; i++)
+    Console.WriteLine(array2[i]);
