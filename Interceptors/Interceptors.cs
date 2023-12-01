42c5defdb2b17f4e1ba864af51f9e14cebb7e37d
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-12-01 06:41:15 +0100

    Interceptors example

---
+++ Interceptors/Interceptors.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace InterceptorsTest
+{
+    public static class CodeGenerator
+    {
+        [InterceptsLocation("""C:\Users\lukas.salficky\Documents\VS\CSharp12_Examples\Interceptors\Program.cs""", line: 7, character: 16)]
+        public static void EnrollInterceptor(this Student studentExample, string StudentName)
+        {
+            Console.WriteLine($"This is a new way of Student {StudentName} registration");
+        }
+    }
+}
--- Interceptors/Program.cs
+++ Interceptors/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Runtime.CompilerServices;
+
+var studentExample = new Student();
+
+studentExample.Enroll("Ali");
+studentExample.Enroll("Hasan");
+studentExample.Enroll("Hasan");
+studentExample.Enroll("Reza");
+
+public class Student
+{
+    public void Enroll(string studentName)
+    {
+        Console.WriteLine($"This Student {studentName} has been registered");
+    }
+}
