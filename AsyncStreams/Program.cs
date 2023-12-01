27904b49bee5060540e4c8805b6401b874fc37ef
Salfický Lukáš <Lukas.Salficky@dako-cz.cz>
2023-12-01 10:08:00 +0100

    ASync stream

---
+++ AsyncStreams/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// Simulation of async stream
+async IAsyncEnumerable<int> GenerateNumbersAsync()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        await Task.Delay(100);
+        yield return i;
+    }
+}
+
+await foreach (var number in GenerateNumbersAsync())
+{
+    Console.WriteLine(number);
+}
+
+
+// Example of parallel for each, so each element is processed in parallel
+await Parallel.ForEachAsync(GenerateNumbersAsync(), async (number, _) =>
+{
+    await ProcessNumberAsync(number);
+});
+Task ProcessNumberAsync(int number) => throw new NotImplementedException();
